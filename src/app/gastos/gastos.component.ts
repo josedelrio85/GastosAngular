bdeefd26e22c463764008bf0aaddc8a1a06ccbfa
warnier <josedelrio@warnier.com>
2018-04-09 11:16:54 +0200

    getFecha para realizar filtrado por fecha y anho getGastosFiltro para llamar a la función de servicio getGastosFiltro se desactiva scrolldown si estan seteados selectores anho y mes se modifican funciones de orden para importe,fecha y entidad. Ahora se tendrá en cuenta, al ordenar, si están seteados los select de mes y anho, y en tal caso, solo se ordenarán los resultados de la fecha seleccionada
    se crea clase ayudante para formato de fecha enviada por post al metodo del servicio

--- src/app/gastos/gastos.component.ts
+++ src/app/gastos/gastos.component.ts
@@ -3,6 +3,8 @@ import { GastosService } from '../services/gastos-service.service';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { Gastos } from '../Modelos/Gastos';
 import { GastosDetailComponent } from './gastos-detail/gastos-detail.component';
+import { Meses } from '../Modelos/meses.enum';
+import { anhos } from '../Modelos/Global';
 
 @Component({
   selector: 'app-gastos',
@@ -21,8 +23,39 @@ export class GastosComponent implements OnInit {
   constructor(private httpService: GastosService, private modalService: NgbModal) {}
 
   ngOnInit() {
+    this.listaAnho = anhos;
     this.getGastos();
   }
+
+  listaAnho: any[] = [];
+  meses = Meses;
+
+  selectorMes: number;
+  selectorAnho: number;
+
+  fechaFiltro: fechaFiltro = new fechaFiltro();
+  
+  getFecha(){
+    if(this.selectorAnho != undefined)
+        this.fechaFiltro.anhos = this.selectorAnho.toString();
+    
+    if(this.selectorMes != undefined)
+      this.fechaFiltro.mes = this.selectorMes.toString();
+
+    if(this.fechaFiltro.anhos != undefined && this.fechaFiltro.mes != undefined){
+      this.getGastosFiltro();
+    }
+  }
+
+
+  getGastosFiltro(){
+    this.httpService.getGastosFiltro(this.fechaFiltro).subscribe(z => {
+
+      this.listaGastos = z;
+    });
+  }
+
+
   rm: number = 15;
   lista: Gastos[];
   listaAux: Gastos[];
@@ -42,21 +75,164 @@ export class GastosComponent implements OnInit {
     });
   }
 
+
   onScrollDown () {
-    if(this.siguienteMuestra < this.lista.length){
-      this.siguienteMuestra += this.rm;
-      this.listaAux = this.lista.slice(this.listaGastos.length, this.siguienteMuestra);
-      this.listaGastos = this.listaGastos.concat(this.listaAux);
-    }else{
-      this.listaAux = this.lista.slice(this.listaGastos.length, this.lista.length);      
-      this.listaGastos = this.listaGastos.concat(this.listaAux);
+    if(this.fechaFiltro.anhos == undefined && this.fechaFiltro.mes == undefined){
+      if(this.siguienteMuestra < this.lista.length){
+        this.siguienteMuestra += this.rm;
+        this.listaAux = this.lista.slice(this.listaGastos.length, this.siguienteMuestra);
+        this.listaGastos = this.listaGastos.concat(this.listaAux);
+      }else{
+        this.listaAux = this.lista.slice(this.listaGastos.length, this.lista.length);      
+        this.listaGastos = this.listaGastos.concat(this.listaAux);
+      }
     }
   }
 
-  
-  getGastosMes(fecha: string): void{
-    this.httpService.getGastosMes(fecha).subscribe(z => this.listaGastos = z);
+
+
+  onSorted($event){
+    // console.log("5 -> GastosComponent onSorted: Se recibe contenido emitido por SortService, asignado a sorted (Ouput) en el proceso de escucha ");
+    //console.log($event.sortColumn);    
+    if($event.sortColumn == "fecha"){
+      this.getGastosByFecha($event.sortDirection);
+    }
+
+    if($event.sortColumn == "importe"){
+      this.getGastosByImporte($event.sortDirection);
+    }
+
+    if($event.sortColumn == "idEntidad"){
+      this.getGastosByEntidad($event.sortDirection);
+    }
   }
+
+  getGastosByFecha(sentido: string): void{
+    if(this.fechaFiltro.anhos != undefined && this.fechaFiltro.mes != undefined){
+      this.ordenFechaConFiltro(sentido);
+    }else{
+      this.ordenFechaSinFiltro(sentido);
+    }    
+  }
+
+  ordenFechaSinFiltro(sentido: string): void{
+    this.httpService.getGastos().subscribe(z => {
+      this.listaGastos = z;
+      if(sentido === "asc"){
+        this.listaGastos.sort((a,b) => new Date(a.fecha).getTime() - new Date(b.fecha).getTime())
+      }else if(sentido === "desc"){
+        this.listaGastos.sort((a,b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime())        
+      }
+    });
+  }
+
+  ordenFechaConFiltro(sentido: string): void{
+    this.httpService.getGastosFiltro(this.fechaFiltro).subscribe(z => {
+      this.listaGastos = z;
+      if(sentido === "asc"){
+        this.listaGastos.sort((a,b) => new Date(a.fecha).getTime() - new Date(b.fecha).getTime())
+      }else if(sentido === "desc"){
+        this.listaGastos.sort((a,b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime())        
+      }
+    });
+  }
+
+
+  getGastosByImporte(sentido: string): void{
+    if(this.fechaFiltro.anhos != undefined && this.fechaFiltro.mes != undefined){
+      this.ordenImporteConFiltro(sentido);
+    }else{
+      this.ordenImporteSinFiltro(sentido);
+    }
+  }
+
+  ordenImporteSinFiltro(sentido: string): void{
+    this.httpService.getGastos().subscribe(z => {
+      this.listaGastos = z;
+      if(sentido === "asc"){
+        this.listaGastos.sort((a,b): any => {
+          if(a.importe < b.importe) return -1;
+          if(a.importe > b.importe) return 1;
+          return 0;
+        });
+      }else if(sentido === "desc"){
+        this.listaGastos.sort((a,b): any => {
+          if(a.importe < b.importe) return 1;
+          if(a.importe > b.importe) return -1;
+          return 0;
+        });
+      }
+    });
+  }
+
+  ordenImporteConFiltro(sentido: string): void{
+    this.httpService.getGastosFiltro(this.fechaFiltro).subscribe(z => {
+      this.listaGastos = z;
+      if(sentido === "asc"){
+        this.listaGastos.sort((a,b): any => {
+          if(a.importe < b.importe) return -1;
+          if(a.importe > b.importe) return 1;
+          return 0;
+        });
+      }else if(sentido === "desc"){
+        this.listaGastos.sort((a,b): any => {
+          if(a.importe < b.importe) return 1;
+          if(a.importe > b.importe) return -1;
+          return 0;
+        });
+      }
+    });
+  }
+
+
+
+  getGastosByEntidad(sentido: string): void{
+    if(this.fechaFiltro.anhos != undefined && this.fechaFiltro.mes != undefined){
+      this.ordenEntidadConFiltro(sentido);
+    }else{
+      this.ordenEntidadSinFiltro(sentido);
+    }
+  }
+
+  ordenEntidadSinFiltro(sentido: string): void{
+    this.httpService.getGastos().subscribe(z => {
+      this.listaGastos = z;
+      if(sentido === "asc"){
+        this.listaGastos.sort((a,b): any => {
+          if(a.idEntidad < b.idEntidad) return -1;
+          if(a.idEntidad > b.idEntidad) return 1;
+          return 0;
+        });
+      }else if(sentido === "desc"){
+        this.listaGastos.sort((a,b): any => {
+          if(a.idEntidad < b.idEntidad) return 1;
+          if(a.idEntidad > b.idEntidad) return -1;
+          return 0;
+        });
+      }
+    });
+  }
+
+  ordenEntidadConFiltro(sentido: string): void{
+    this.httpService.getGastosFiltro(this.fechaFiltro).subscribe(z => {
+      this.listaGastos = z;
+      if(sentido === "asc"){
+        this.listaGastos.sort((a,b): any => {
+          if(a.idEntidad < b.idEntidad) return -1;
+          if(a.idEntidad > b.idEntidad) return 1;
+          return 0;
+        });
+      }else if(sentido === "desc"){
+        this.listaGastos.sort((a,b): any => {
+          if(a.idEntidad < b.idEntidad) return 1;
+          if(a.idEntidad > b.idEntidad) return -1;
+          return 0;
+        });
+      }
+    });
+  }
+
+
 
   delete(item: Gastos): void{
     let index = this.listaGastos.indexOf(item);
@@ -87,69 +263,14 @@ export class GastosComponent implements OnInit {
     this.showNew = estado;
   }
 
-  getGastosByFecha(sentido: string): void{
-    this.httpService.getGastos().subscribe(z => {
-      this.listaGastos = z;
-      if(sentido === "asc"){
-        this.listaGastos.sort((a,b) => new Date(a.fecha).getTime() - new Date(b.fecha).getTime())
-      }else if(sentido === "desc"){
-        this.listaGastos.sort((a,b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime())        
-      }
-    });
-  }
-
-  getGastosByImporte(sentido: string): void{
-    this.httpService.getGastos().subscribe(z => {
-      this.listaGastos = z;
-      if(sentido === "asc"){
-        this.listaGastos.sort((a,b): any => {
-          if(a.importe < b.importe) return -1;
-          if(a.importe > b.importe) return 1;
-          return 0;
-        });
-      }else if(sentido === "desc"){
-        this.listaGastos.sort((a,b): any => {
-          if(a.importe < b.importe) return 1;
-          if(a.importe > b.importe) return -1;
-          return 0;
-        });
-      }
-    });
-  }
-
-  getGastosByEntidad(sentido: string): void{
-    this.httpService.getGastos().subscribe(z => {
-      this.listaGastos = z;
-      if(sentido === "asc"){
-        this.listaGastos.sort((a,b): any => {
-          if(a.idEntidad < b.idEntidad) return -1;
-          if(a.idEntidad > b.idEntidad) return 1;
-          return 0;
-        });
-      }else if(sentido === "desc"){
-        this.listaGastos.sort((a,b): any => {
-          if(a.idEntidad < b.idEntidad) return 1;
-          if(a.idEntidad > b.idEntidad) return -1;
-          return 0;
-        });
-      }
-    });
-  }
+  //para gastos-mes.component
+  getGastosMes(fecha: string): void{
+    this.httpService.getGastosMes(fecha).subscribe(z => this.listaGastos = z);
+  }  
+}
 
 
-  onSorted($event){
-    // console.log("5 -> GastosComponent onSorted: Se recibe contenido emitido por SortService, asignado a sorted (Ouput) en el proceso de escucha ");
-    //console.log($event.sortColumn);    
-    if($event.sortColumn == "fecha"){
-      this.getGastosByFecha($event.sortDirection);
-    }
-
-    if($event.sortColumn == "importe"){
-      this.getGastosByImporte($event.sortDirection);
-    }
-
-    if($event.sortColumn == "idEntidad"){
-      this.getGastosByEntidad($event.sortDirection);
-    }
-  }
+export class fechaFiltro{
+  anhos: string;
+  mes: string;
 }
